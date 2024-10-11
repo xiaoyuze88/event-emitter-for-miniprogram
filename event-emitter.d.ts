18446d76175c78db86a2de9aa3142500ff4838df
vinsonxiao <vinsonxiao@tencent.com>
2024-10-11 12:51:26 +0800

    feat: improve event defines typing

--- event-emitter.d.ts
+++ event-emitter.d.ts
@@ -1,9 +1,15 @@
-type listenerCallback = (argus: any) => any;
+export type EventDefines = Record<string, any>;
+export type EventName<T extends EventDefines> = string & keyof T;
+export type EventListener<T> = (params: T) => void;
+export type EventListenerResponseMap<T extends string> = Partial<Record<T, any>>;
 
-export default class EventEmitter {
+export default class EventEmitter<
+  Defines extends EventDefines = any,
+  Response extends EventListenerResponseMap<EventName<Defines>> = any,
+> {
   constructor();
-  on: (type: string, listener: listenerCallback) => EventEmitter;
-  once: (type, listener: listenerCallback) => EventEmitter;
-  off: (type, listener?: listenerCallback) => EventEmitter;
-  emit: (type, ...argus) => Promise<any>;
+  on: <Name extends EventName<Defines>>(type: Name, listener: EventListener<Defines[Name]>) => EventEmitter;
+  once: <Name extends EventName<Defines>>(type: Name, listener: EventListener<Defines[Name]>) => EventEmitter;
+  off: <Name extends EventName<Defines>>(type: Name, listener?: EventListener<Defines[Name]>) => EventEmitter;
+  emit: <Name extends EventName<Defines>>(type: Name, params: Defines[Name]) => Promise<Array<Response[Name]>>;
 }
